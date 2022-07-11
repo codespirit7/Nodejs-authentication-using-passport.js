e239761042da0f83870cd28c4efc774b2c32ab76
Ritesh Singh <singhriteshs1429@gmail.com>
2022-07-11 14:17:48 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI : "mongodb+srv://ritesh:741852Rite$h@cluster0.takyk.mongodb.net/?retryWrites=true&w=majority"
+    MongoURI : "mongodb+srv://<username>:<password>@cluster0.takyk.mongodb.net/?retryWrites=true&w=majority"
 }
